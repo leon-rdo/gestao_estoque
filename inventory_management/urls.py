c8d7e54f44d4d02e3ecf03c521e687c81c06afaf
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-01-26 15:44:48 -0300

    refactor: add app_name to urls.py

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import *
 
+app_name = 'inventory_management'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path(),
+    # path(),
     path('produto/<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('produto/unidade/<uuid:pk>/', ProductUnitDetailView.as_view(), name='product_unit_detail')
 ]
