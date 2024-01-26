c1d3d144895c6dded30b98208eb39a0703601b99
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-01-26 17:06:05 -0300

    feat: add link to category list

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'inventory_management'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    # path(),
+    path('categorias/', CategoryListView.as_view(), name='category_list'),
     path('produto/<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('produto/unidade/<uuid:pk>/', ProductUnitDetailView.as_view(), name='product_unit_detail')
 ]
