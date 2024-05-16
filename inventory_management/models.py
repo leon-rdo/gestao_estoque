b213006ebcae1e4044aa9e507a67b8ce4d304743
Thiago Menezes <thimenezes19@gmail.com>
2024-05-16 15:42:21 -0300

    fix: fix str stockTransfer in models

--- inventory_management/models.py
+++ inventory_management/models.py
@@ -213,7 +213,9 @@ class StockTransfer(models.Model):
         super(StockTransfer, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.product_unit.product.name} - {self.origin_shelf.hall.room.building} ( {self.origin_shelf}) -> {self.destination_shelf.hall.room.building} ({self.destination_shelf})"
+        if self.origin_shelf:
+            return f'{self.product_unit.product.name} - {self.origin_shelf} -> {self.destination_shelf}'
+        return f'{self.product_unit.product.name} - {self.origin_transfer_area} -> {self.destination_transfer_area} - {self.destination_shelf}'
 
     class Meta:
         verbose_name_plural = "Transferências de Estoque"
