a9d5bd5b1250614c9750d3f38c367dc3a82fdf7b
Thiago Menezes <thimenezes19@gmail.com>
2024-05-24 08:13:21 -0300

    fix: correct names

--- inventory_management/apps.py
+++ inventory_management/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
-def create_trasnfer_area(sender, **kwargs):
+def create_transfer_area(sender, **kwargs):
     from .models import TransferAreas
     TransferAreas.objects.get_or_create(name='Loja')
     TransferAreas.objects.get_or_create(name='Depósito')
@@ -13,4 +13,4 @@ class InventoryManagementConfig(AppConfig):
     verbose_name = 'Gestão de Estoque'
 
     def ready(self):
-        post_migrate.connect(create_trasnfer_area, sender=self)
+        post_migrate.connect(create_transfer_area, sender=self)
